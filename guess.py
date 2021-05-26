6c8935c42519a64804442573fe7054f60f813f8c
student <rahmafaizyusuf@gmail.com>
2021-05-26 16:19:56 +0300

    first commit

---
+++ guess.py
@@ -0,0 +1,35 @@
+import random
+
+def guess_number(r):
+    random_number=random.randint(1, r)
+    guess=0
+    while guess != random_number:
+        guess=int(input("Guess a number between 1 and {r}"))
+        if guess < random_number:
+            print("Sorry, too low, guess again")
+        elif guess > random_number:
+            print("Sorry, too high, guess again")
+
+    print("Congrats you have guessed the number {random_number} correctly")
+
+def computer_guess(r):
+    low=1
+    high=r
+    feedback=""
+    while feedback != "c":
+        if low != high:
+           guess=random.randint(low, high)
+        else:
+            guess=low
+        feedback=input(f"Is {guess} too high (H) too low (L) or correct (c) !!").lower()
+        if feedback=="h":
+            high=guess-1
+    
+        elif feedback=="l":
+            low=guess+1
+    print("Congrats! The computer guessed your number {guess} correctly")
+
+computer_guess(10)          
+
+        
+    
---
+++ project1.py
@@ -0,0 +1,8 @@
+adj=input("Adjective: ")
+verb1=input("verb1: ")
+verb2=input("verb2: ")
+famous_person=input("famous_person: ")
+
+madlib=f"computer programming is so {adj} ! It makes me so excited because I love to {verb1}. Stay hydrated and {verb2} \
+    like you are {famous_person} "
+
